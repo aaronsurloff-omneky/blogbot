1f05eec1b863c07cd673872ae5da80caf72cc242
aaronsurloff-omneky <132226084+aaronsurloff-omneky@users.noreply.github.com>
2023-05-02 22:40:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from langchain.agents import initialize_agent
 from langchain.agents import AgentType
 
 openai_api_key = st.secrets["openai_apikey"]
-serpapi_apikey = st.secrets["serpapi_apikey"]
 google_api_key = st.secrets["google_api_key"]
+google_api_key = st.secrets["google_cse_id"]
 google_cse_id = st.secrets["google_cse_id"]
 
 
@@ -39,7 +39,7 @@ blog_memory = ConversationBufferMemory(input_key='title', memory_key='chat_histo
 
 ## LLMS
 llm = OpenAI(temperature=0.9, max_tokens = 1000, openai_api_key = openai_api_key)
-tools = load_tools(["google-search"], llm=llm, serpapi_api_key = serpapi_apikey)
+tools = load_tools(["google-search"], llm=llm, google_api_key = google_api_key)
 agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
 title_chain = LLMChain(llm=llm, prompt = title_template, verbose = True, output_key='title', memory=title_memory)
